f5bf65a5254ad9236110cf989995aefa807c4542
calisthenicsGuy <radevradoslav12@gmail.com>
2022-03-27 13:02:36 +0300

    Tasks added

---
+++ 11. Text Processing/P03.Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P03.Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string substringToRemove = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int indexOfSubstringInText = text.IndexOf(substringToRemove);
+
+            while (indexOfSubstringInText != -1)
+            {
+                text = text.Remove(indexOfSubstringInText, substringToRemove.Length);
+                indexOfSubstringInText = text.IndexOf(substringToRemove);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
