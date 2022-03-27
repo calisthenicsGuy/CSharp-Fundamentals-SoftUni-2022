35926f7b97cb9c3f4df39bf639dfe6e9374e6955
calisthenicsGuy <radevradoslav12@gmail.com>
2022-03-27 15:09:38 +0300

    Tasks added

---
+++ 15. Exam Preparation/P01.Activation_Keys/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_Activation_Keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Generate")
+            {
+                string[] commandArgs = command
+                    .Split(">>>", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string action = commandArgs[0];
+
+                if (action == "Contains")
+                {
+                    string substring = commandArgs[1];
+                    if (!activationKey.Contains(substring))
+                    {
+                        Console.WriteLine($"Substring not found!");
+                        continue;
+                    }
+                    Console.WriteLine($"{activationKey} contains {substring}");
+                }
+                else if (action == "Flip")
+                {
+                    int startIndex = int.Parse(commandArgs[2]);
+                    int endIndex = int.Parse(commandArgs[3]);
+
+                    string substringToChage = "";
+                    string sizeOfLetters = commandArgs[1];
+                    if (sizeOfLetters == "Upper")
+                    {
+                        substringToChage = activationKey.Substring(startIndex, endIndex - startIndex);
+                        substringToChage = substringToChage.ToUpper();
+                    }
+                    else if (sizeOfLetters == "Lower")
+                    {
+                        substringToChage = activationKey.Substring(startIndex, endIndex - startIndex);
+                        substringToChage = substringToChage.ToLower();
+                    }
+
+                    activationKey = activationKey.Replace(activationKey.Substring(startIndex, endIndex - startIndex), substringToChage);
+
+                    Console.WriteLine(activationKey);
+                }
+                else if (action == "Slice")
+                {
+                    int startIndex = int.Parse(commandArgs[1]);
+                    int endIndex = int.Parse(commandArgs[2]);
+
+                    activationKey = activationKey.Remove(startIndex, endIndex-startIndex);
+                    Console.WriteLine(activationKey);
+                }
+            }
+
+            Console.WriteLine($"Your activation key is: {activationKey}");
+        }
+    }
+}
---
+++ 15. Exam Preparation/Task-Emoji_Detector/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Task_Emoji_Detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([*]{2}|[:]{2})+([A-Z][a-z]{2,})\1";
+            string newpattern = @"[0-9]+";
+
+            string text = Console.ReadLine();
+
+            double total = 1;
+            double emoji = 0;
+
+            MatchCollection matches = Regex.Matches(text, pattern);
+            MatchCollection num = Regex.Matches(text, newpattern);
+
+            List<string> totalEmoji = new List<string>();
+
+            foreach (Match item in num)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    total *= int.Parse(item.Value[i].ToString());
+                }
+
+            }
+
+            foreach (Match item in matches)
+            {
+                string emo = item.Groups[2].Value;
+                for (int i = 0; i < emo.Length; i++)
+                {
+                    emoji += (double)emo[i];
+                }
+                if (emoji > total)
+                {
+                    totalEmoji.Add(item.Value);
+                }
+                emoji = 0;
+            }
+            Console.WriteLine($"Cool threshold: {total}");
+            Console.WriteLine($"{matches.Count} emojis found in the text. The cool ones are:");
+            Console.Write(string.Join("\n", totalEmoji));
+        }
+    }
+}
