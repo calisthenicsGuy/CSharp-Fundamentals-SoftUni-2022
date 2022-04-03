c346502ce9ffabac50f548f9f3c10335430c8f85
calisthenicsGuy <radevradoslav12@gmail.com>
2022-04-03 22:56:12 +0300

    Tasks

---
+++ Associative_Arrays-More_Exercise/P01.Ranking/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> courses = new Dictionary<string, string>();
+            Dictionary<string, Dictionary<string, int>> competitors = new Dictionary<string, Dictionary<string, int>>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end of contests")
+            {
+                string[] contestInfo = input
+                    .Split(":", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string contest = contestInfo[0];
+                string contestPassword = contestInfo[1];
+
+                courses[contest] = contestPassword;
+            }
+
+            int bestCandidateScore = 0;
+            input = null;
+            while ((input = Console.ReadLine()) != "end of submissions")
+            {
+                string[] competitorsInfo = input
+                    .Split("=>", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string contest = competitorsInfo[0];
+                string password = competitorsInfo[1];
+                string username = competitorsInfo[2];
+                int points = int.Parse(competitorsInfo[3]);
+
+                if (!courses.ContainsKey(contest))
+                {
+                    continue;
+                }
+                if (courses[contest] != password)
+                {
+                    continue;
+                }
+
+                if (competitors.ContainsKey(username) && competitors[username].ContainsKey(contest))
+                {
+                    if (points > competitors[username][contest])
+                    {
+                        competitors[username][contest] = points;
+                    }
+                    continue;
+                }
+
+                if (!competitors.ContainsKey(username))
+                {
+                    competitors[username] = new Dictionary<string, int>();
+                }
+                competitors[username].Add(contest, points);
+            }
+
+            string bestCandidateName = null;
+            int bestScore = 0;
+
+            BestCandidate(bestCandidateName, bestScore, competitors);
+            Output(competitors, bestCandidateName, bestScore);
+        }
+
+        private static void BestCandidate(string bestCandidateName, int bestScore, Dictionary<string, Dictionary<string, int>> competitors)
+        {
+            foreach (var competitor in competitors.OrderBy(n => n.Key))
+            {
+                int tempBestPoints = 0;
+                foreach (var competitorScore in competitor.Value.OrderByDescending(p => p.Value))
+                {
+                    tempBestPoints += competitorScore.Value;
+                }
+
+                if (tempBestPoints > bestScore)
+                {
+                    bestScore = tempBestPoints;
+                    bestCandidateName = competitor.Key;
+                }
+            }
+
+            Console.WriteLine($"Best candidate is {bestCandidateName} with total {bestScore} points.");
+        }
+
+        public static void Output(Dictionary<string, Dictionary<string, int>> competitors, string bestCandidateName, int bestScore)
+        {
+            Console.WriteLine("Ranking: ");
+            foreach (var competitor in competitors.OrderBy(n => n.Key))
+            {
+                Console.WriteLine(competitor.Key);
+                foreach (var competitorScore in competitor.Value.OrderByDescending(p => p.Value))
+                {
+                    Console.WriteLine($"#  {competitorScore.Key} -> {competitorScore.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Associative_Arrays-More_Exercise/P04.Dragon_Army/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.Dragon_Army
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Dragon> dragons = new List<Dragon>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] parameters = Console.ReadLine().Split(' ');
+                double value = 0;
+
+                Dragon dragon = new Dragon()
+                {
+                    Type = parameters[0],
+                    Name = parameters[1],
+                    Damage = double.TryParse(parameters[2], out value) ? value : 45,
+                    Health = double.TryParse(parameters[3], out value) ? value : 250,
+                    Armour = double.TryParse(parameters[4], out value) ? value : 10,
+                };
+
+                Dragon existingDragon = dragons.SingleOrDefault(d => d.Name == dragon.Name && d.Type == dragon.Type);
+                if (existingDragon != null)
+                {
+                    existingDragon.Damage = dragon.Damage;
+                    existingDragon.Health = dragon.Health;
+                    existingDragon.Armour = dragon.Armour;
+                }
+                else
+                {
+                    dragons.Add(dragon);
+                }
+            }
+
+            var groupedDragons = dragons.GroupBy(
+                d => d.Type,
+                d => d,
+                (key, d) =>
+                    new GroupedDragon()
+                    {
+                        Type = key,
+                        Dragons = d.ToList()
+                    });
+
+            foreach (GroupedDragon groupedDragon in groupedDragons)
+            {
+                int dragonCount = groupedDragon.Dragons.Count;
+                double damage = groupedDragon.Dragons.Sum(d => d.Damage) / dragonCount;
+                double health = groupedDragon.Dragons.Sum(d => d.Health) / dragonCount;
+                double armour = groupedDragon.Dragons.Sum(d => d.Armour) / dragonCount;
+
+                Console.WriteLine($"{groupedDragon.Type}::({damage:F2}/{health:F2}/{armour:F2})");
+
+                foreach (Dragon dragon in groupedDragon.Dragons.OrderBy(d => d.Name))
+                {
+                    Console.WriteLine($"-{dragon.Name} -> damage: {dragon.Damage}, health: {dragon.Health}, armor: {dragon.Armour}");
+                }
+            }
+        }
+    }
+
+    class Dragon
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public double Damage { get; set; }
+        public double Health { get; set; }
+        public double Armour { get; set; }
+    }
+
+    class GroupedDragon
+    {
+        public string Type { get; set; }
+        public List<Dragon> Dragons { get; set; }
+    }
+}
